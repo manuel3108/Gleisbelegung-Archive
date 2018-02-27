e7ca9c9dd1bc7b982dca93b221e6a8fab1ac100b
Unknown <manuel-serret@t-online.de>
2018-02-27 19:53:29 +0100

    #55 implementiert

--- Plugin Gleisbelegung/src/com/gleisbelegung/Plugin.java
+++ Plugin Gleisbelegung/src/com/gleisbelegung/Plugin.java
@@ -19,10 +19,19 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Plugin extends Application implements Runnable{
     private Stage primaryStage;
@@ -116,6 +125,7 @@ public class Plugin extends Application implements Runnable{
             }
         }
 
+        primaryStage.setOnCloseRequest(we -> checkLogOnClosing());
         this.primaryStage = primaryStage;
 
         u = new Update();
@@ -147,6 +157,7 @@ public class Plugin extends Application implements Runnable{
         }*/
 
         einstellungen = new Einstellungen();
+        setOutputStreams();
     }
 
     private void startLoading(){
@@ -178,6 +189,193 @@ public class Plugin extends Application implements Runnable{
             }
         };
         new Thread(r).start();
+    }
+
+    private void setOutputStreams(){
+        try{
+            File f = new File(Einstellungen.appOrdner + File.separator + "Log.txt");
+
+            if(!f.exists()){
+                f.createNewFile();
+            }
+
+            if(f.exists() && f.canRead()){
+                System.out.println("Fehler und Meldungen werden in log-Datei geschrieben! Speicherort: " + f.getAbsolutePath());
+
+                FileOutputStream fos = new FileOutputStream(f, false);
+                PrintStream ps = new PrintStream(fos);
+                System.setErr(ps);
+                System.setOut(ps);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void checkLogOnClosing(){
+        try {
+            File f = new File(Einstellungen.appOrdner + File.separator + "Log.txt");
+
+            RandomAccessFile raf = new RandomAccessFile(f, "rw");
+
+            raf.writeBytes("java Exception");
+            raf.close();
+
+
+
+            raf = new RandomAccessFile(f, "rw");
+            String temp = raf.readLine();
+            while(temp != null && !temp.contains("java") && !temp.contains("com.") && !temp.contains("GITHUB")){
+                temp = raf.readLine();
+            }
+
+            if(temp != null && (temp.contains("java") || temp.contains("com.") || temp.contains("GITHUB"))){
+                if(u.getNeusteVersion() <= version){
+                    openLogWindow();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void openLogWindow(){
+        Stage stage = new Stage();
+
+        //Label l = new Label("Während deiner aktuellen Sitzung sind Fehler aufgetreten. Durch einen Klick auf Weiter werden deine Log-Datei und deine Anregungen anonym hochgeladen.");
+        Label l = new Label("Während deiner aktuellen Sitzung sind Fehler aufgetreten. Durch einen Klick auf Weiter wird deine Log-Datei anonym hochgeladen.");
+        l.setStyle("-fx-text-fill: white");
+        l.setFont(Font.font(Einstellungen.schriftgroesse));
+        l.setWrapText(true);
+        l.setMaxWidth(450);
+        l.setTranslateY(25);
+        l.setTranslateX(25);
+
+        /*TextField ta = new TextField();
+        ta.setFont(Font.font(settingsFontSize-3));
+        ta.setTranslateX(25);
+        ta.setTranslateY(125);
+        ta.setPrefWidth(450);
+        ta.setPrefHeight(100);*/
+
+
+        Button bno = new Button("Abbrechen");
+        bno.setFont(Font.font(Einstellungen.schriftgroesse));
+        bno.setOnAction(e -> stage.close());
+        bno.setTranslateX(250);
+        bno.setTranslateY(150);
+
+        Button byes = new Button("Weiter");
+        byes.setFont(Font.font(Einstellungen.schriftgroesse));
+        byes.setTranslateX(150);
+        byes.setTranslateY(150);
+        byes.setOnAction(e -> {
+            byes.setDisable(true);
+            bno.setDisable(true);
+
+            Runnable r = () -> {
+                sendLogFile(l);
+            };
+            new Thread(r).start();
+        });
+
+        Pane p = new Pane(l, byes, bno);
+        p.setStyle("-fx-background-color: #303030");
+        p.setMinSize(500,200);
+        p.setMaxSize(500, 200);
+
+        Scene s = new Scene(p,500,200);
+
+
+        stage.setScene(s);
+        stage.setTitle("Log-Datei senden?");
+
+        stage.setAlwaysOnTop(true);
+        stage.show();
+    }
+
+    private void sendLogFile(Label l){
+        try {
+            Platform.runLater(() -> l.setText("Lese Log-Datei..."));
+
+            File f = new File(Einstellungen.appOrdner + File.separator + "Log.txt");
+
+            RandomAccessFile raf = new RandomAccessFile(f, "r");
+            String newLine = raf.readLine();
+            String log = newLine;
+            while(newLine != null){
+                log += "\n" + newLine;
+                newLine = raf.readLine();
+            }
+
+            Platform.runLater(() -> l.setText("Lade hoch..."));
+
+            String baseUrl = "http://manuel-serret.bplaced.net/Gleisbelegung/request.php";
+            //String baseUrl = "http://localhost/Webseiten/Gleisbelegung/request.php";
+
+
+            int counter = 0;
+            char[] c = log.toCharArray();
+            ArrayList<String> logArray = new ArrayList<>();
+            logArray.add("");
+
+            for (int i = 0; i < log.length(); i++) {
+                logArray.set(counter, logArray.get(counter) + c[i]);
+
+                if(logArray.get(counter).length() >= 2000 - 10){ //-10 wegen versionsnummer
+                    logArray.set(counter, logArray.get(counter).replace(" ","%20"));
+                    logArray.set(counter, logArray.get(counter).replace("\n","%0A"));
+
+                    counter++;
+                    logArray.add(counter, "");
+                }
+            }
+            logArray.set(counter, logArray.get(counter).replace(" ","%20"));        //für das letzte element
+            logArray.set(counter, logArray.get(counter).replace("\n","%0A"));
+
+            URL url = new URL( baseUrl + "?action=new&message=v." + version + "&log=" + URLEncoder.encode(logArray.get(0),java.nio.charset.StandardCharsets.UTF_8.toString()));
+            URLConnection con = url.openConnection();
+            con.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
+
+            Scanner sc = new Scanner(con.getInputStream());
+            int id = Integer.parseInt(sc.nextLine());
+
+            counter = 0;
+            while (counter < logArray.size()) {
+                try{
+                    final int temp = counter;
+                    Platform.runLater(() -> l.setText("Lade " + temp + " von " + (logArray.size()-1) + " hoch..."));
+
+                    url = new URL(baseUrl + "?action=add&id="+id+"&log=" + URLEncoder.encode(logArray.get(counter), StandardCharsets.UTF_8.toString()));
+                    con = url.openConnection();
+                    con.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
+
+                    con.getInputStream();
+
+                    counter++;
+                } catch (Exception e){
+                    e.printStackTrace();
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+
+            Platform.runLater(() -> l.setText("Fertig. Vielen Dank für deine Hilfe!"));
+            Runnable r = () -> {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Platform.exit();
+            };
+            new Thread(r).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
