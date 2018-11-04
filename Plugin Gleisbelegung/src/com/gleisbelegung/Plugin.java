65d555d0f886446df494ecb97984b0641819bf1e
Manuel <manuel-serret@t-online.de>
2018-11-04 20:05:17 +0100

    Änderung der Versionsnummer

--- Plugin Gleisbelegung/src/com/gleisbelegung/Plugin.java
+++ Plugin Gleisbelegung/src/com/gleisbelegung/Plugin.java
@@ -39,7 +39,7 @@ public class Plugin extends Application implements Runnable{
     private Fenster f;
 
     private String host = "192.168.1.25";                   //Die Ip des Rechnsers, auf welchem die Sim läuft           (Wird bei einer Änderung beim Pluginstart aktualisiert)
-    private int version = 16;                                //Aktualle Version des Plugins
+    private int version = 17;                                //Aktualle Version des Plugins
 
     private Stellwerk stellwerk;
     public static Einstellungen einstellungen;
